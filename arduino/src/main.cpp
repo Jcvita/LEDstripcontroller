e62e3169254f60150f261b03a2514eee6362a746
Joseph Vita <jcvita2012@gmail.com>
2020-09-05 16:20:14 -0400

    fix conflict

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,19 +1,35 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <Wire.h>  //using I2C to chat with PI 0
 
+// #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#define FASTLED_ALLOW_INTERRUPTS 0
 
+int incomingByte = 0;
 CRGB leds[600];
 
 void setup() {
   delay(3000);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(74880);
   FastLED.addLeds<NEOPIXEL, 6>(leds, 600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
+  
   if (Serial.available() > 0){
+      incomingByte = Serial.read();
 
+      Serial.print("I received: ");
+      Serial.println(incomingByte);
+      if (incomingByte == 102) {
+        Serial.println("Paid respects.");
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(200);
+        digitalWrite(LED_BUILTIN, HIGH);
+      }
+      incomingByte = 0;
   }
   
 }
