34aefe2979440271dd21360d470f5bc181e99884
Jcvita <jcvita2012@gmail.com>
2020-09-08 16:39:59 -0400

    include old attempt using flask & the neopixel library

---
+++ old_attempt/app.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3.8
+from flask import Flask, render_template, request, redirect, url_for
+import requests, json
+import utils
+
+app = Flask(__name__)
+
+@app.route("/")
+def site():
+    return render_template('index.html')
+
+
+@app.route("/alert/<rgb>", methods=["POST"])
+def alert(rgb):
+    utils.do_action(action="alert", color=rgb) 
+    return redirect(url_for("/"))
+
+
+@app.route("/rainbow/<freq>", methods=["POST"])
+def rainbow(freq):
+    utils.do_action(action="rainbow", frequency=freq)
+    return redirect(url_for("/"))
+
+@app.route("/fill/<rgb>", methods=["POST"))
+def fill(rgb):
+    utils.do_action(action="fill", color=rgb)
+    return redirect(url_for("/"))
+
+@app.route("/party/<freq>", methods=["POST"])
+def party(freq):
+    utils.do_action(action="party", frequency=freq)
+    return redirect(url_for("/"))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
