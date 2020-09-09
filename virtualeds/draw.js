ade8b865c1939116adb24005e54f18809ec37ba3
Joseph Vita <jcvita2012@gmail.com>
2020-09-09 17:18:03 -0400

    canvas

--- virtualeds/draw.js
+++ virtualeds/draw.js
@@ -1,20 +1,23 @@
-const canvas = document.querySelector(".canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
 const scale = 10;
 const rows = 1;
 const columns = canvas.width / scale;
 var leds = [];
-var num_leds = 10;
+var num_leds = 60;
 
 
-function setup() {
+(function setup() {
     for (var i = 0; i < num_leds; i++){
-        leds.push(new Led());
+        leds.push(new Led(i));
     }
     leds.forEach(element => element.draw())
 
     window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //update LEDs
+        leds.forEach(element => element.setRGB(255, 0, 0))
+        
         leds.forEach(element => element.update())
-    })
-}
+    }, 250);
+}());
--- virtualeds/led.js
+++ virtualeds/led.js
@@ -1,13 +1,13 @@
-function Led() {
+function Led(col) {
     this.r = 0;
     this.g = 0;
     this.b = 0;
 
     this.row = 0
-    this.col = 0
+    this.col = col
 
     this.draw = function() {
-        ctx.fillStyle = "#FFFFFF";
+        ctx.fillStyle = "#000000";
         ctx.fillRect(this.row, this.col, scale, scale);
     }
 
