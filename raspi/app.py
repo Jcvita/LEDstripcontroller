698e69e46be9ff72cf1e3b7544d76cdb59ea4cb9
Joseph Vita <jcvita2012@gmail.com>
2020-09-09 07:04:41 -0400

    Server communicates with Arduino over serial

--- raspi/app.py
+++ raspi/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
+import utils
 
 app = Flask(__name__)
 
@@ -11,12 +12,21 @@ def static_rgb():
     #send rgb data to utils file that sends data to arduino
     if request.method == "POST":
         data = request.get_json()
-
-        print(data)
         
+        utils.send_static_rgb(data['red'], data['green'], data['blue'])
+
         return jsonify(data)
         
     # return redirect(url_for("site"))
+
+@app.route("/partymode", methods=["POST"])
+def partymode():
+    if request.method == "POST":
+        data = request.get_json()
+
+        utils.party_mode(int(data['freq']))
+
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
--- raspi/utils.py
+++ raspi/utils.py
@@ -1,3 +1,18 @@
 import time
 import random
 import serial
+
+ser = serial.Serial('/dev/ttyS0', 115200)
+
+def send_static_rgb(r: str, g: str, b: str):
+    r = '00'[:3-len(r)] + r
+    g = '00'[:3-len(r)] + g  # padding for data formatting
+    b = '00'[:3-len(r)] + b
+
+    ser.write(f"RGB>{r + g + b}")
+
+def party_mode(freq: int):
+    while True:
+        send_static_rgb(str(random.randint(0, 254)), str(random.randint(0, 254)), str(random.randint(0, 255)))
+        time.sleep(1000/freq)
+        
