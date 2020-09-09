55052e5e530b678199d04f95a0553f3ae8243d86
Joseph Vita <jcvita2012@gmail.com>
2020-09-09 06:04:04 -0400

    fastLED command for RGB fill + circuit diagram

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,33 +2,40 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
-// #include <Wire.h>  //using I2C to chat with PI 0
 
 // #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
 // #define FASTLED_ALLOW_INTERRUPTS 0
 #define NUM_LEDS 200 // leds are in groups of 3 ~ 600/3
 #define DATA_PIN 6
 
-int incomingByte = 0;
+String incomingString = "";
 
 CRGB leds[200];
 
 void setup() {
   Serial.begin(74880);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  FastLED.setBrightness(128);
 }
 
 void loop() {
   
   if (Serial.available() > 0){
-      incomingByte = Serial.read();
-
-      Serial.print("I received: ");
-      Serial.println(incomingByte);
-      leds[incomingByte] = CHSV(CRGB::Red, 255, 255);
-      FastLED.show();
-      incomingByte = 0;
+      incomingString = Serial.readString();  //format: [cmd]>[data]
+      if (incomingString.substring(0, incomingString.indexOf('>')) == "RST"){
+          FastLED.clear();
+      }
+      else if (incomingString.substring(0, incomingString.indexOf('>')) == "RGB"){       
+          int r = incomingString.substring(incomingString.indexOf('>') + 1, incomingString.indexOf('>') + 4).toInt();
+          int g = incomingString.substring(incomingString.indexOf('>') + 4, incomingString.indexOf('>') + 7).toInt();
+          int b = incomingString.substring(incomingString.indexOf('>') + 7, incomingString.indexOf('>') + 10).toInt();
+          for (int x = 0; x < NUM_LEDS; x++){
+            leds[x].setRGB(r, g, b);
+          }
+          FastLED.show();
+      }
+      
+      
+      
   }
   
 }
