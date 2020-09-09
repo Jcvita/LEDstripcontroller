b6399cf99bc7b8136640fae3120d14bc15ea1eb8
Joseph Vita <jcvita2012@gmail.com>
2020-09-09 05:06:44 -0400

    fixed commented code

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,61 +1,34 @@
-// // #define FASTLED_INTERRUPT_RETRY_COUNT 1
+// #define FASTLED_INTERRUPT_RETRY_COUNT 1
 
-// #include <Arduino.h>
-// #include <FastLED.h>
-// // #include <Wire.h>  //using I2C to chat with PI 0
-
-// // #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
-// // #define FASTLED_ALLOW_INTERRUPTS 0
-// #define NUM_LEDS 600
-// #define DATA_PIN 6
-
-// int incomingByte = 0;
-
-// CRGB leds[600];
-
-// void setup() {
-//   Serial.begin(74880);
-//   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-//   FastLED.setBrightness(128);
-// }
-
-// void loop() {
-  
-//   if (Serial.available() > 0){
-//       incomingByte = Serial.read();
-
-//       Serial.print("I received: ");
-//       Serial.println(incomingByte);
-//       leds[incomingByte] = CHSV(CRGB::Red, 255, 255);
-//       FastLED.show();
-//       incomingByte = 0;
-//   }
-  
-// }
+#include <Arduino.h>
 #include <FastLED.h>
+// #include <Wire.h>  //using I2C to chat with PI 0
 
-#define Num_Strip1 200
+// #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+// #define FASTLED_ALLOW_INTERRUPTS 0
+#define NUM_LEDS 200 // leds are in groups of 3 ~ 600/3
+#define DATA_PIN 6
 
-CRGB Strip1[Num_Strip1];
+int incomingByte = 0;
+
+CRGB leds[200];
 
 void setup() {
-  FastLED.addLeds<WS2811,6>(Strip1, Num_Strip1);
+  Serial.begin(74880);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(128);
 }
 
 void loop() {
-  for(int a = 0; a < 10; )  {
-    Strip1[a] = CHSV(255, 255, 255);
-    a++;
-    Strip1[a] = CHSV(155, 255, 255);
-    FastLED.show();
-    delay(100);
-  }
+  
+  if (Serial.available() > 0){
+      incomingByte = Serial.read();
 
-  for(int a = 10; a > 0; )  {
-    Strip1[a] = CHSV(255, 255, 255);
-    a--;
-    Strip1[a] = CHSV(155, 255, 255);
-    FastLED.show();
-    delay(100);
+      Serial.print("I received: ");
+      Serial.println(incomingByte);
+      leds[incomingByte] = CHSV(CRGB::Red, 255, 255);
+      FastLED.show();
+      incomingByte = 0;
   }
+  
 }
