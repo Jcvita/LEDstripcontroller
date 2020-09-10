b57b449222f7836265b2b589c94e2ffbcd61a851
Joseph Vita <jcvita2012@gmail.com>
2020-09-09 20:30:13 -0400

    virtualed solid rgb

--- virtualeds/draw.js
+++ virtualeds/draw.js
@@ -4,7 +4,7 @@ const scale = 10;
 const rows = 1;
 const columns = canvas.width / scale;
 var leds = [];
-var num_leds = 60;
+var num_leds = 100;
 
 (function setup() {
     document.getElementById("canvas").width = scale*num_leds;
@@ -15,11 +15,35 @@ var num_leds = 60;
 
     window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
         //manipulate LEDs
-        leds.forEach(element => element.setRGB(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)));
+        if (getCookie("action") == "party"){
+            leds.forEach(element => element.setRGB(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)));
+        } else if (getCookie("action") == "rgbsliders"){
+            leds.forEach(element => element.setRGB(getCookie("r"), getCookie("g"), getCookie("b")));
+        } else if (getCookie("action") == "cmdenter") {
+            leds.forEach(element => element.setRGB(0, 0, 0));
+        }
+        
         //update LEDs
         for (var i = 0; i < num_leds; i++){
             leds[i].update(i);
         }
     }, 500);
 }());
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
